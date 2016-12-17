a9ea7e640369bb61511447cb0a82bfb43c588d1d
BhavanaR <bhavanar2003@gmail.com>
2016-12-17 01:04:18 -0500

    Updated vector representation of answer. Added prediction code within Train class

--- LSTM_Train_SQuAD.py
+++ LSTM_Train_SQuAD.py
@@ -5,24 +5,27 @@ from keras.layers.recurrent import LSTM
 from squad_dataset import get_squad_data
 from keras.preprocessing import sequence
 from keras.layers.embeddings import Embedding
-from keras.layers import Dropout
+from keras.layers import Dropout,RepeatVector,Merge
+from keras.callbacks import EarlyStopping
 import json
-def train_test_split(X,Y, test_size=0.1):
+
+def train_test_split(X,Xq,Y, test_size=0.1):
     """
     This just splits data to training and testing parts
     """
     ntrn = int(len(X) * (1 - test_size))
 
     X_train = X[:ntrn,:]
+    Xq_train = Xq[:ntrn,:]
     Y_train = Y[:ntrn,:]
+    
     X_test = X[ntrn:,:]
+    Xq_test = Xq[ntrn:,:]
     Y_test = Y[ntrn:,:]
-    print(X_train.shape, Y_train.shape, X_test.shape, Y_test.shape)
-    return (X_train, Y_train), (X_test, Y_test)
+    #print(X_train.shape, Y_train.shape, X_test.shape, Y_test.shape)
+    return (X_train, Xq_train, Y_train), (X_test, Xq_test, Y_test)
 
-X,Y,vocab = get_squad_data()
-X = X[:10000]
-Y = Y[:10000]
+X,Xq,Y,vocab = get_squad_data(size=2000)
 
 word_to_int = dict((word, i+1) for i, word in enumerate(vocab))
 int_to_word = dict((i, word) for word, i in word_to_int.items())
@@ -33,57 +36,100 @@ with open('IntToWordMap.json','w') as file:
 n_vocab = len(vocab)
 print("Total Vocab: ", n_vocab)
 
-seq_length = len(max(X,key=len))
-ans_seq_length = len(max(Y,key=len))
+context_length = len(max(X,key=len))
+ques_length = len(max(Xq,key=len))
+ans_length = len(max(Y,key=len))
 
-print(seq_length,ans_seq_length)
+#print(context_length,ans_length)
 #print(X[0],Y[0])
 # prepare the dataset of input to output pairs encoded as integers
 dataX = []
+dataXq = []
 dataY = []
 for i in range(0, len(X)):
-	dataX.append([word_to_int[word] for word in X[i]])
-	dataY.append([word_to_int[word] for word in Y[i]])
+    dataX.append([word_to_int[word] for word in X[i]])
+    dataXq.append([word_to_int[word] for word in Xq[i]])
+    
+    answer_indexes = [word_to_int[word] for word in Y[i]]  
+    #print(answer_indexes,Y[i])
+    Y_vector = np.zeros(len(vocab)+1,dtype=np.uint32)
+    Y_vector[answer_indexes] = 1
+    dataY.append(Y_vector)
+    
+dataX = np.array(sequence.pad_sequences(dataX,maxlen=context_length),dtype=np.uint32)
+dataXq = np.array(sequence.pad_sequences(dataXq,maxlen=ques_length),dtype=np.uint32)
+dataY = np.array(dataY,dtype=np.uint32)
 
-dataX = np.array(sequence.pad_sequences(dataX,maxlen=seq_length))
-dataY = np.array(sequence.pad_sequences(dataY,maxlen=ans_seq_length))
-#print(dataX[0],dataY[0])
-
-n_patterns = len(dataX)
-print("Total Patterns: ", n_patterns)
+print("DataX shape : {0}, DataXq shape : {1}, DataY shape : {2}".format(dataX.shape,dataXq.shape,dataY.shape))
+#print("DataX sample: {0},DataY sample : {1}".format(dataX[0],dataY[0]))
 
 test_size = 0.2
-(X_train, Y_train) , (X_test, Y_test) =  train_test_split(dataX,dataY,test_size)
-print(X_train.shape, Y_train.shape, X_test.shape, Y_test.shape)
+(X_train, Xq_train, Y_train) , (X_test, Xq_test, Y_test) =  train_test_split(dataX,dataXq,dataY,test_size)
+print("X_train shape : {0}, Xq_train shape : {1}, Y_train shape : {2}".format(X_train.shape,Xq_train.shape,Y_train.shape))
+print("X_test shape : {0}, Xq_test shape : {1}, Y_test shape : {2}".format(X_test.shape,Xq_test.shape,Y_test.shape))
 
 # define the LSTM model
-hidden_neurons = 100
-
 embedding_vector_length = 32
-
-#How to give a different dimensions for in and out
-model = Sequential()
-model.add(Embedding(n_vocab,embedding_vector_length, input_length=seq_length))
-model.add(LSTM(hidden_neurons, return_sequences=False))
-model.add(Dropout(0.3))
-model.add(Dense(ans_seq_length, activation='softmax'))
-model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
-print (model.summary())
-
 epochs = 100
-batch_size = 50
-print("Epoch : {0}, Neurons : {1}, Batch_size : {2}, ContextLength : {3}, AnswerLength : {4}".format(epochs,hidden_neurons,batch_size,seq_length,ans_seq_length))
+batch_size = 64
+embed_hidden_size = 150
 
-print("Running {0} epochs".format(epochs))
-model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=epochs, validation_data=(X_test,Y_test))
+sentrnn = Sequential()
+sentrnn.add(Embedding(len(vocab)+1, embed_hidden_size,
+                      input_length=context_length))
+sentrnn.add(Dropout(0.3))
 
-model.save('squad_linear_{0}_epoch.h5'.format(i))
+qrnn = Sequential()
+qrnn.add(Embedding(len(vocab)+1, embed_hidden_size,
+                   input_length=ques_length))
+qrnn.add(Dropout(0.3))
+qrnn.add(LSTM(embed_hidden_size, return_sequences=False))
+qrnn.add(RepeatVector(context_length))
 
-predicted = model.predict(X_test)
+model = Sequential()
+model.add(Merge([sentrnn, qrnn], mode='sum'))
+model.add(LSTM(embed_hidden_size, return_sequences=False))
+model.add(Dropout(0.3))
+model.add(Dense(len(vocab)+1, activation='softmax'))
+model.compile(loss="categorical_crossentropy", optimizer="rmsprop", metrics=['accuracy'])
+#print (model.summary())
+
+callbacks = [EarlyStopping(monitor='val_loss', min_delta=0.00001, patience=0, verbose=0, mode='auto')]
+
+print("Epoch : {0}, Neurons : {1}, Batch_size : {2}, ContextLength : {3}, AnswerLength : {4}".format(epochs,embed_hidden_size,batch_size,context_length,ans_length))
+
+model.fit([X_train,Xq_train], Y_train, batch_size=batch_size, nb_epoch=epochs, validation_split=0.1,callbacks=callbacks)
+
+#model.save('squad_linear_{0}_epoch.h5'.format(epochs))
+
 #rmse = np.sqrt(((predicted - Y_test) ** 2).mean(axis=0))
 #print("RMSE : ",rmse)
 
-scores = model.evaluate(X_test,Y_test, verbose=0)
+scores = model.evaluate([X_test,Xq_test],Y_test, verbose=1)
 print("Accuracy: ",scores )
 print("Accuracy: %.2f%%" % (scores[1]*100))
+
+predictedY = model.predict([X_test,Xq_test])
+
+print("Predictions:")
+#len(predictedY)
+for i in range(0,3):
+    #print(predictedY[i])
+    sorted_values = predictedY[i].argsort()[-3:][::-1]
+    #answer_indexes = np.nonzero(predictedY[i])[0]
+    answer_indexes = sorted_values
+    print(answer_indexes)
+    predictedWords = []
+    predictedWords = [int_to_word[int(num)] for num in answer_indexes if num != 0]
+    
+    if(len(predictedWords)>0):
+        print("Answers are:")
+        #for i in range(0,len(Y)+1):
+        print("Context:")
+        print(X[i])
+        print("Answer:")
+        print(Y[i])
+        print("Predicted:")
+        print(predictedWords)
+    
 print("Done")
--- squad_dataset.py
+++ squad_dataset.py
@@ -1,27 +1,38 @@
 import json
 
-def get_squad_data():
+def get_squad_data(size=None):
     correct_file_name = "Correct_Sentences.json"
 
     with open(correct_file_name) as data_file:
         correct_sentences = json.load(data_file)
     vocab = set()
     X = []
+    Xq = []
     Y = []
 
+    if size != None:
+        count = 0
     for key in correct_sentences.keys():
         question = correct_sentences.get(key)
+        question_tokens = question.get("question")
+        ground_truths = question.get("ground_truths")       
+        vocab.update(set(question_tokens))   
         for sentence in question.get("sentences"):
-            sentence_question_tokens = sentence  + question.get("question")
-            ground_truths = question.get("ground_truths")
-            vocab.update(set(sentence_question_tokens))
+            vocab.update(set(sentence))
             for ground_truth in ground_truths:
-                X.append(sentence_question_tokens)
+                X.append(sentence)
+                Xq.append(question_tokens)
                 Y.append(ground_truth)
                 vocab.update(set(ground_truth))
-
-    print(len(X), len(Y), len(vocab))
-    return X,Y,vocab
+                count+=1
+                if count == size:
+                    break
+            if count == size:
+                break
+        if count == size:
+                break
+    print(len(X), len(Xq), len(Y), len(vocab))
+    return X,Xq,Y,vocab
 
 if __name__ == '__main__':
-    X,Y,vocab = get_squad_data()
+    X,Xq,Y,vocab = get_squad_data(10)
